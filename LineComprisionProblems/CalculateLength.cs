1aae15e7dfa28a6e5959d3fb4ee507345d715af3
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-04-28 16:31:29 +0530

    [Calculate length of line]

--- LineComprisionProblems/CalculateLength.cs
+++ LineComprisionProblems/CalculateLength.cs
@@ -8,7 +8,10 @@ namespace LineComparision
 {
     public class CalculateLength
     {
-        int x1, y1, x2, y2;
+        private int x1;
+        private int y1;
+        private int x2;
+        private int y2;
         public CalculateLength(int x1, int y1, int x2, int y2)
         {
             this.x1 = x1;
@@ -16,27 +19,11 @@ namespace LineComparision
             this.x2 = x2;
             this.y2 = y2;
         }
-        public double calculate()
+
+        public double GetLength()
         {
-            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            return length;
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
         }
-        public void equalCheck(double line1, double line2)
-        {
-            if (line1.Equals(line2))
-                Console.WriteLine("Both lines are equal");
-            else
-                Console.WriteLine("Lines are not equal");
-        }
-        public void lineCompare(double line1, double line2)
-        {
-            if (line1.CompareTo(line2) > 0)
-                Console.WriteLine("Line1 is greater than line1");
-            else if (line1.CompareTo(line2) < 0)
-                Console.WriteLine("Line2 is greater than line1");
-            else
-                Console.WriteLine("Both lines are equal");
-        }
-    }
+    }    
 
 }
